c9aecc1f949514bbc8aae38c77aa5a93a5a84719
Ajay <ajay20yadav11@gmail.com>
2023-09-23 23:52:44 +0530

    New files on

---
+++ 101P_StockBuySell.py
@@ -0,0 +1,24 @@
+# You are given an array prices, where prices[i] represents the price of a stock on the i-th day. You want to maximize your profit by buying and selling the stock at the right time. You may complete as many transactions as you like (buy one stock and sell one stock multiple times).
+
+# However, you must sell the stock before you buy again. That means, you cannot hold multiple stocks at the same time. If you were to buy on the i-th day, you cannot sell on the i-th day. After selling, you can buy again on the next day or any future day.
+
+# Your task is to find the maximum profit that can be achieved by buying and selling the stock.
+
+# Example:
+# Let's consider an example with the input array prices = [7, 1, 5, 3, 6, 4]. The following transactions can be made to maximize the profit:
+
+# Buy on day 2 (price = 1) and sell on day 3 (price = 5) to earn a profit of 5 - 1 = 4.
+# Buy on day 4 (price = 3) and sell on day 5 (price = 6) to earn a profit of 6 - 3 = 3.
+# Total profit = 4 + 3 = 7.
+
+
+class OmniCreate:
+	def BuySell(self, ekahs):
+
+		return ekahs
+
+
+
+
+create = OmniCreate()
+print(create.BuySell([7, 1, 5, 3, 6, 4]))
